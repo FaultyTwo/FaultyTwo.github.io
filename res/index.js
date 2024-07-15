2d2b3842db2bdb029210d7adb1619f8d8a8606eb
FaultyTwo <mapcast42@gmail.com>
2024-07-15 17:36:41 +0700

    enlarge function

--- res/index.js
+++ res/index.js
@@ -16,10 +16,43 @@ function resetPage(){
 let enlargeBool = false;
 function enlargeInfo(){
     let dom = document.querySelector(".grid")
-    if(enlargeBool){
-        dom.style.gridTemplateColumns = "50% 50%";
-    } else {
-        dom.style.gridTemplateColumns = "30% 70%";
+    let domlgrid = document.querySelector("#l-grid")
+    let domcontent = document.querySelector("#content")
+    let isSmallScreen = screen.width <= 768 || window.innerWidth <= 768;
+
+    if(!isSmallScreen){ // avoid changing dom when mobile ui is in usage
+        if(enlargeBool){
+            domlgrid.style.display = 'block';
+            dom.style.gridTemplateColumns = "50% 50%";
+            domcontent.style.gridTemplateColumns= '50% 50%';
+        } else {
+            domlgrid.style.display = 'none';
+            dom.style.gridTemplateColumns = "30% 70%";
+            domcontent.style.gridTemplateColumns= '100%';
+        }
+        enlargeBool = !enlargeBool;
     }
-    enlargeBool = !enlargeBool;
 }
+
+function widthMobile(){
+    let enlargeDom = document.querySelector(".enlarge")
+    if(enlargeDom == null){
+        return;
+    } 
+
+    if(screen.width <= 768 || window.innerWidth <= 768){
+        enlargeDom.style.opacity = 0.5;
+        enlargeDom.style.cursor = "not-allowed"
+    } else {
+        enlargeDom.style.opacity = 1.0;
+        enlargeDom.style.cursor = "pointer"
+    }
+}
+
+window.addEventListener('load', () => {
+    widthMobile()
+})
+
+window.addEventListener('resize', () => {
+    widthMobile()
+})
