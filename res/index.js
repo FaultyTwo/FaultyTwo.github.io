303e1a0abc3024e40125f2722576309cd32ff712
FaultyTwo <mapcast42@gmail.com>
2024-07-15 17:02:04 +0700

    added enlarge button + restructure

--- res/index.js
+++ res/index.js
@@ -1,5 +1,3 @@
-
-
 function changePage(page_name){
     let resource = `res/subpages/${page_name}.html`;
     let dom = document.getElementById("info-iframe");
@@ -13,4 +11,15 @@ function resetPage(){
     let dom = document.getElementById("info-iframe");
     dom.src = ""
     changePage("greeting")
+}
+
+let enlargeBool = false;
+function enlargeInfo(){
+    let dom = document.querySelector(".grid")
+    if(enlargeBool){
+        dom.style.gridTemplateColumns = "50% 50%";
+    } else {
+        dom.style.gridTemplateColumns = "30% 70%";
+    }
+    enlargeBool = !enlargeBool;
 }
