7e0c1f5572cdfadef5343fe0ab82a3ed83d0082a
FaultyTwo <mapcast42@gmail.com>
2024-11-26 16:48:38 +0700

    where can i drink them idea juice ???

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 document.querySelector("#current_year").textContent = new Date().getFullYear().toString()
+
+const validFiles = ["about"]
+
+function toPage(name){
+    const newSrc = `public/subpages/${name}.html`;
+    document.querySelector("#content-iframe").src = newSrc;
+}
+
+document.querySelector("#nav-home").addEventListener('click', (e) => {
+    toPage('home')
+})
+
+document.querySelector("#nav-about").addEventListener('click', (e) => {
+    toPage('about')
+})
